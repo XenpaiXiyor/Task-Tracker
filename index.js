9c68e1f12d64e5456b5f0680aa75ea74cf0ca1df
Rhythm Mittal <rhythmmittal19@gmail.com>
2025-11-12 16:28:50 +0530

    task_tracker

---
+++ index.js
@@ -0,0 +1,52 @@
+// functionalities
+/*
+  1. add
+  2. update
+  3. delete
+  4. mark a task as in progress / done
+  5. list all tasks that are done / not done / in progress
+
+*/
+
+console.log(process.argv);
+
+//records the command
+let command = process.argv[2]?.toLowerCase();
+
+// records the value like "Buy groceries"
+let value = process.argv[3];
+
+
+// add files needed to manage these functions
+const { addTask, updateTask, deleteTask, markDone, markProgress, listTask } = require('./taskManager');
+
+
+switch (command) {
+  case 'add':
+    addTask(value);
+    break;
+
+  case 'update':
+    updateTask(value);
+    break;
+
+  case 'delete':
+    deleteTask(value);
+    break;
+
+  case 'mark-done':
+    markDone(value);
+    break;
+
+  case 'mark-in-progress':
+    markProgress(value);
+    break;
+
+  case 'list':
+    listTask(value);
+    break;
+
+  default:
+    console.log("❌ Unknown command. Try 'add', 'update', 'delete', 'mark-done', 'mark-in-progress', or 'list'.");
+}
+
---
+++ taskManager.js
@@ -0,0 +1,43 @@
+const fs = require("fs"); // fs = filesystem
+const filePath = "./tasks.json";
+
+// Helper function → ensures file exists and returns current tasks
+function loadTasks() {
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]", "utf8");
+    console.log("File Created!");
+    return []; // start with an empty array
+  } else {
+    try {
+      const data = fs.readFileSync(filePath, "utf8");
+      const tasks = JSON.parse(data);
+      return tasks; // return the parsed array
+    } catch (err) {
+      console.error("Error reading tasks:", err);
+      return [];
+    }
+  }
+}
+
+// // Helper function → saves updated tasks back
+function saveTasks(tasks) {}
+
+// // Core functionalities
+// function addTask(description) { ... }
+// function updateTask(id, newDescription) { ... }
+// function deleteTask(id) { ... }
+// function markDone(id) { ... }
+// function markProgress(id) { ... }
+// function listTask(status) { ... }
+
+// Export List
+
+module.exports = {
+  loadTasks,
+  addTask,
+  updateTask,
+  deleteTask,
+  markDone,
+  markProgress,
+  listTask,
+};
